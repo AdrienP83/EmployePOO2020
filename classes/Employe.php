c514d91ddb7b149faed1159ed4b870a7e83ec9be
Adrien Poignard <poignard.adrien@gmail.com>
2020-10-08 10:28:12 +0200

    Création de la classe emplye et traitement avec la 1er methode

---
+++ classes/Employe.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Employe
+ *
+ * @author adrien.poignard
+ */
+class Employe {
+    protected  int $numero;
+    protected string $nom;
+    protected string $prenom;
+    protected DateTime $dateDeNaissance;
+    protected float $salaireM;
+    
+    /**
+     * Constructeur de la classe Employe
+     * @param int $numero
+     * @param string $nom
+     * @param string $prenom
+     * @param DateTime $dateDeNaissance
+     * @param float $salaireM
+     */
+    function __construct(int $numero, string $nom, string $prenom, DateTime $dateDeNaissance, float $salaireM) {
+        $this->numero = $numero;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->dateDeNaissance = $dateDeNaissance;
+        $this->salaireM = $salaireM;
+    }
+    function getNumero(): int {
+        return $this->numero;
+    }
+
+    function getNom(): string {
+        return $this->nom;
+    }
+
+    function getPrenom(): string {
+        return $this->prenom;
+    }
+
+    function getDateDeNaissance(): DateTime {
+        return $this->dateDeNaissance;
+    }
+
+    function getSalaireM(): float {
+        return $this->salaireM;
+    }
+
+    function setSalaireM(float $salaireM): void {
+        $this->salaireM = $salaireM;
+    }
+
+
+    
+}
---
+++ includes/Traitement.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Traitement
+ *
+ * @author adrien.poignard
+ */
+class Traitement {
+    public static function instanciationUnEmploye () : void{
+        $e = new Employe (1,"Dupont","Jacques",new DateTime("12/07/1980"),1000.00);
+        $nom = $e ->getNom();
+        echo "L'employe instancié s'appelle" . $nom;
+    }
+}
